21fc17cdac78dff10a37dc75560e00e7d1d2522f
ImadNait <naitmihoubimzd@gmail.com>
2024-08-05 20:06:22 +0100

    Initial commit

--- Authentication/server.js
+++ Authentication/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT;
 const authRoutes = require('./routes/authRoute');
 const app = express();
+// middlewares
 app.use(express.json())
-
+app.use(cookieParser())
 app.use(express.static('public'));
 
 
@@ -27,3 +29,23 @@ mongoose.connect(dbURL, { dbName:"NFC" })
 app.get('/',(req, res)=>res.render ('home'))
 app.get('/list',(req, res)=>res.render('list'))
 app.use(authRoutes);
+
+
+//cookies
+app.get('/set-cookies',(req, res)=>{
+
+    res.cookie('nigga', true)
+    res.cookie('isCustomer', true, { maxAge: 1000*60*60*24, httpOnly:true })
+
+    res.send('u got cookies!')
+
+})
+
+app.get('/read-cookies',(req, res)=>{
+    const cookies = req.cookies;
+    console.log(cookies.nigga)
+
+    res.json(cookies);
+
+
+})
